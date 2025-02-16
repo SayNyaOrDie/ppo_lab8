fb943f01d03393062e99b2911f9771262d287c86
skitter <sharvad_skitter@mail.ru>
2025-02-17 00:30:10 +0300

    feat: нихуя у меня не получилось
    при попытке вызова /deposit кидает ошибку на существующем аккаунте либо делай через тесты либо хуй знает
    С sagaContext ничего не тестил соответственно, завтра как приду буду помогать тебе

--- tiny-event-sourcing-spring-app/src/main/kotlin/ru.quipy/bankDemo/accounts/controller/AccountController.kt
+++ tiny-event-sourcing-spring-app/src/main/kotlin/ru.quipy/bankDemo/accounts/controller/AccountController.kt
@@ -4,21 +4,36 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import ru.quipy.bankDemo.accounts.api.AccountAggregate
 import ru.quipy.bankDemo.accounts.api.AccountCreatedEvent
 import ru.quipy.bankDemo.accounts.api.BankAccountCreatedEvent
+import ru.quipy.bankDemo.accounts.api.BankAccountDepositEvent
 import ru.quipy.bankDemo.accounts.logic.Account
 import ru.quipy.bankDemo.accounts.logic.BankAccount
+import ru.quipy.bankDemo.transfers.api.TransferTransactionAggregate
+import ru.quipy.bankDemo.transfers.api.TransferTransactionCreatedEvent
+import ru.quipy.bankDemo.transfers.logic.TransferTransaction
 import ru.quipy.core.EventSourcingService
+import ru.quipy.saga.SagaManager
+import java.math.BigDecimal
 import java.util.*
 
 @RestController
 @RequestMapping("/accounts")
 class AccountController(
-    val accountEsService: EventSourcingService<UUID, AccountAggregate, Account>
+    val accountEsService: EventSourcingService<UUID, AccountAggregate, Account>,
+    val transferEsService: EventSourcingService<UUID, TransferTransactionAggregate, TransferTransaction>,
+    val sagaManager: SagaManager
 ) {
+    val transferSagaName = "TRANSFER_PROCESSING"
 
+
+//    holderId != accountId
+//    holder - пользователь системы
+//    account - аккаунт holder'а, может быть сколько угодно
+//    bankAccount - банковский счёт, привязанный к account (макс 5)
     @PostMapping("/{holderId}")
     fun createAccount(@PathVariable holderId: UUID) : AccountCreatedEvent {
         return accountEsService.create { it.createNewAccount(holderId = holderId) }
@@ -37,5 +52,30 @@ class AccountController(
     @GetMapping("/{accountId}/bankAccount/{bankAccountId}")
     fun getBankAccount(@PathVariable accountId: UUID, @PathVariable bankAccountId: UUID) : BankAccount? {
         return accountEsService.getState(accountId)?.bankAccounts?.get(bankAccountId)
+    }
+
+    @PostMapping("/{bankAccountId}/deposit")
+    fun deposit(@PathVariable bankAccountId: UUID, @RequestParam amount: BigDecimal): BankAccountDepositEvent {
+        return accountEsService.create { it.deposit(bankAccountId, amount) }
+    }
+
+//    Перевод между аккаунтами двух РАЗНЫХ пользователей
+    @PostMapping("/{accountFromId}/transfer")
+    fun transferToAccount(
+        @PathVariable accountFromId: UUID,
+        @RequestParam accountToId: UUID,
+        @RequestParam amount: BigDecimal
+    ): TransferTransactionCreatedEvent {
+        val accountFrom = accountEsService.getState(accountFromId)
+        val accountTo = accountEsService.getState(accountToId)
+
+        val sagaContext = sagaManager
+            .launchSaga(transferSagaName, "start processing")
+            .sagaContext()
+//    в примере sagaContext без (), но у меня ругается
+//        return transferEsService.create(sagaContext) {
+//            it.initiateTransferTransaction(sagaContext.ctx[transferSagaName]!!.sagaInstanceId, accountFrom.accountId)
+//        }
+    return TODO();
     }
 }
